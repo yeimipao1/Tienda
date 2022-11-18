65dd667497b3ae3174b1062ad2c367c8f9956920
yeimipao <yeimipao1@gmail.com>
2022-11-17 21:56:13 -0300

    problemas

--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,41 +1,74 @@
 import React, { Fragment } from 'react'
+import "../../App.css"
+import { Link } from "react-router-dom"
+import { Search } from './Search'
+import { useDispatch, useSelector } from 'react-redux'
+import { useAlert } from 'react-alert'
+import { logout} from "../../actions/userActions"
 
 const Header = () => {
-  return (
-    <Fragment>
-        <nav className='navbar row'>
-            <div className='col-12 col-md-3'>
-                <div className='navbar-brand'>
-                    <img src="./imagenes/logo1.png" alt="Navegantes Store Logo"></img>
-                </div>
-            </div>
+    const {cartItems} = useSelector(state=>state.cart)
+    
+    const alert= useAlert();
+    const dispatch= useDispatch();
 
-            <div className='col-12 col-md-6 mt-2 mt-md-0'>
-                <div className="input-group">
-                        <input
-                            type="text"
-                            id="search_field"
-                            class="form-control"
-                            placeholder='Que producto busca?'></input>
-                            <div class="input-group-append">
-                                <button id="search-btn" class="btn">
-                                    <i class=" fa fa-search-plus fa-2x text-white"  aria-hidden="true"></i>
-                                </button>
-                            </div>
-                            
+    const { user, loading } = useSelector(state => state.auth)
+
+    const logoutHandler = () =>{
+        dispatch(logout());
+        alert.success("LogOut exitoso")
+    }
+    return (
+        <Fragment>
+            <nav className='navbar row'>
+                <div className='col-12 col-md-3'>
+                    <div className='navbar-brand'>
+                        <Link to="/" ><img src="../imagenes/logo1.png" alt="Navegantes Store Logo"></img></Link>
                     </div>
                 </div>
-                <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                    <span><button type="button" class="btn btn-light">Inicie sección</button></span>
-                    &nbsp;&nbsp;
-                    <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="true"></i>
-                    <span className="ml-1" id="cart_count">2</span>
+
+                <div className='col-12 col-md-5 mt-2 mt-md-0'>
+                    {/*Aqui va buscar*/}
+                    <Search />
+                </div>
+                {/*Boton inicio sesión*/}
+                <div className="col-12 col-md-4 mt-4 mt-md-0 text-center">
+                    <Link to="/carrito"><i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="false"></i>
+                        <span className="ml-1" id="cart_count">{cartItems.length}</span></Link>
+
+                    {user ? (
+                        <div className="ml-4 dropdown d-inline">
+                            <Link to="#!" className="btn dropdown-toggle text-white mr-4" type="button"
+                                id="dropDownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <figure className='avatar avatar-nav'>
+                                    <img
+                                        src={user.avatar && user.avatar.url}
+                                        alt={user && user.nombre}
+                                        className="rounded-circle"></img>
+                                </figure>
+                                <span>{user && user.nombre}</span>
+                            </Link>
+                            <div className='dropdown-menu' aria-labelledby='dropDownMenu'>
+                                {/*Preguntamos el rol de quien esta online*/}
+                                {user && user.role === "admin" && (
+                                    <Link className="dropdown-item" to="/dashboard">Adm. Productos</Link>
+                                )}
+
+                                <Link className="dropdown-item" to="/">Pedidos</Link>
+                                <Link className="dropdown-item" to="/yo">Mi Perfil</Link>
+                                <Link className="dropdown-item" to="/" onClick={logoutHandler}>Cerrar Sesion</Link>
+                            </div>
+                        </div>
+                    ) : !loading && <Link to="/login" className='btn ml-4' id="login_btn">Login</Link>}
+
+
                 </div>
 
-        </nav>
+            </nav>
 
-    </Fragment>
-  )
+        </Fragment>
+    )
+
 }
 
 export default Header
